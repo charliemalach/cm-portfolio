cf64529ec0f55b2828567d07f6adcbdcb00a3983
Charlie <charlesmalach@gmail.com>
2022-03-24 10:33:42 -0500

    changed javascript

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Programmer", "Editor"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Nerd"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Programmer", "Editor"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Nerd"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
