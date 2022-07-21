33dff4c51ae5e0e1fafd8bcd6e8c9776cc17149f
Charlie <charlesmalach@gmail.com>
2022-07-21 09:43:01 -0500

    Update script.js
    - changed "Computer Nerd" to "Computer Wizard"

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Designer", "Computer Nerd"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Wizard"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Designer", "Computer Nerd"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Wizard"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
