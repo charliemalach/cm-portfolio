6daf00cdee886869bff234c6c772bccaa6417814
Charlie <charlesmalach@gmail.com>
2022-09-02 11:07:03 -0500

    Update script.js
    wizard -> analyst

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Designer", "Computer Wizard"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Designer", "Computer Wizard"],
+        strings: ["Programmer", "Developer", "Designer", "Computer Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
