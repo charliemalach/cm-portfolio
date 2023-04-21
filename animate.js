4b61f1e05641e8cc53c084815191df03cd716745
Charlie <87348126+charliemalach@users.noreply.github.com>
2023-04-21 13:48:43 -0500

    styling changes
    -opacity change
    -image bigger on hover
    -text sizing fix

---
+++ animate.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      // Get the position of the top of the window
+      var scrollTop = $(window).scrollTop();
+  
+      // Get the position of the element you want to animate
+      var elementOffset = $('#image-container').offset().top;
+  
+      // Set the distance you want the user to scroll before the animation triggers
+      var distance = 1000;
+  
+      // Check if the user has scrolled past the element
+      if (scrollTop > elementOffset - distance) {
+        // Animate image to move from left to center of screen
+        $('#image-container').animate({
+          left: '20%' // center of screen
+        }, 500); // adjust speed as needed (in milliseconds)
+      }
+    });
+  });
+  
---
+++ fade.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    var myImage = document.getElementById('my-image');
+    myImage.style.opacity = 1;
+  });
+  
